da1115531d94e4a9f9eb35b93adb05b26ccaee27
Bartek <bartoszb125@gmail.com>
2020-11-04 16:04:15 +0100

    Dodanie plikow z strona, navbar i menu rozwijane

---
+++ _vti_cnf/TabScript.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|03 Nov 2020 17:48:10 -0000
+vti_author:SR|BARTEK-LAPTOP\\Bartek
+vti_modifiedby:SR|BARTEK-LAPTOP\\Bartek
+vti_nexttolasttimemodified:TW|03 Nov 2020 17:39:23 -0000
+vti_timecreated:TR|03 Nov 2020 17:48:10 -0000
+vti_cacheddtm:TX|03 Nov 2020 17:48:10 -0000
+vti_filesize:IR|778
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Mikrus/Index.html
